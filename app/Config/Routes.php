f5e1030ae86e7108e879e77335dc03e6cc33df04
kuntowck <kunto312002@gmail.com>
2025-02-13 17:19:26 +0700

    Mini Project 2: add home and about pages, update routing and controller methods

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,17 +3,22 @@
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Produk;
 use App\Controllers\Pesanan;
+use App\Controllers\User;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/home', function () {
+    return redirect()->to('/');
+});
+$routes->get('/about', 'Home::about');
 
 $routes->get('/produk', [Produk::class, 'index']);
 $routes->get('/produk/detail/(:num)', [Produk::class, 'detail/$1']);
 $routes->get('/produk/create', [Produk::class, 'create']);
 $routes->post('/produk/create', [Produk::class, 'store']);
-$routes->get('/produk/edit/(:num)', [Produk::class, 'edit']);
+$routes->get('/produk/edit/(:num)', [Produk::class, 'edit/$1']);
 $routes->put('/produk/update', [Produk::class, 'update']);
 $routes->delete('/produk/delete/(:num)', [Produk::class, 'delete/$1']);
 
@@ -23,4 +28,4 @@ $routes->get('/pesanan/create', [Pesanan::class, 'create']);
 $routes->post('/pesanan/create', [Pesanan::class, 'store']);
 $routes->get('/pesanan/update/(:num)', [Pesanan::class, 'editStatus/$1']);
 $routes->post('/pesanan/update/', [Pesanan::class, 'updateStatus']);
-$routes->get('/pesanan/delete/(:num)', [Pesanan::class, 'delete/$1']);
+$routes->delete('/pesanan/delete/(:num)', [Pesanan::class, 'delete/$1']);
