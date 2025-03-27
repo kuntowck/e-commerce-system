88e1ce4b7a280d4c1ec7997bcaf89907b851cee8
kuntowck <kunto312002@gmail.com>
2025-03-27 21:25:23 +0700

    update sidebar styles and add logo image

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -1,11 +1,11 @@
 <?php if (in_groups('admin')): ?>
     <div class="bg-white p-4 rounded-lg shadow-sm">
         <ul class="space-y-2">
-            <li><a href="<?= base_url('/admin/dashboard'); ?>" class="block py-2 px-3 font-medium text-gray-900 rounded hover:bg-gray-100 focus:bg-gray-200 hover:text-blue-700">Dashboard</a></li>
-            <li><a href="<?= base_url('/admin/users'); ?>" class="block py-2 px-3 font-medium text-gray-900 rounded hover:bg-gray-100 focus:bg-gray-200 hover:text-blue-700">Users</a></li>
-            <li><a href="<?= base_url('/admin/roles'); ?>" class="block py-2 px-3 font-medium text-gray-900 rounded hover:bg-gray-100 focus:bg-gray-200 hover:text-blue-700">Roles</a></li>
-            <li><a href="<?= base_url('/product-manager/products'); ?>" class="block py-2 px-3 font-medium text-gray-900 rounded hover:bg-gray-100 focus:bg-gray-200 hover:text-blue-700">Products</a></li>
-            <li><a href="<?= base_url('/admin/users/report'); ?>" class="block py-2 px-3 font-medium text-gray-900 rounded hover:bg-gray-100 focus:bg-gray-200 hover:text-blue-700">Reports</a></li>
+            <li><a href="<?= base_url('/admin/dashboard'); ?>" class="block py-2 px-3 font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-200">Dashboard</a></li>
+            <li><a href="<?= base_url('/admin/users'); ?>" class="block py-2 px-3 font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-200">Users</a></li>
+            <li><a href="<?= base_url('/admin/roles'); ?>" class="block py-2 px-3 font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-200">Roles</a></li>
+            <li><a href="<?= base_url('/product-manager/products'); ?>" class="block py-2 px-3 font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-200">Products</a></li>
+            <li><a href="<?= base_url('/admin/users/report'); ?>" class="block py-2 px-3 font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-200">Reports</a></li>
         </ul>
     </div>
 <?php endif; ?>
